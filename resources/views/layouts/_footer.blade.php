a951ae417b99674c754f8f20a5ded017eb5c0a2d
vagrant <vagrant@homestead>
2018-09-25 06:24:31 +0000

    基础布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="https://github.com/w594533/laravel-shop" target="_blank">533</a> 编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:w594533@163.com">联系我</a></p>
+    </div>
+</footer>
