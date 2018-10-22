9cb03a6f924b1efaf75facec6f70bdde4da6bf89
vagrant <vagrant@homestead>
2018-10-22 06:05:26 +0000

    管理后台 - 商品列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -9,9 +9,9 @@ Route::group([
     'namespace'     => config('admin.route.namespace'),
     'middleware'    => config('admin.route.middleware'),
 ], function (Router $router) {
-
     $router->get('/', 'HomeController@index');
 
     $router->resource('users', 'UserController');
 
+    $router->resource('products', 'ProductsController');
 });
