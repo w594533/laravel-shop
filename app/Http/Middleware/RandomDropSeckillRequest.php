91d0c2613ed760401645841b31470394c5db639c
vagrant <vagrant@homestead>
2019-02-10 07:40:49 +0000

    随机拒绝

--- app/Http/Middleware/RandomDropSeckillRequest.php
+++ app/Http/Middleware/RandomDropSeckillRequest.php
@@ -17,9 +17,9 @@ class RandomDropSeckillRequest
     public function handle($request, Closure $next, $percent)
     {
         // $percent 参数是在路由添加中间件时传入
-        if (random_int(0, 100) < (int)$percent) {
-            throw new InvalidRequestException('参与的用户过多，请稍后再试', 403);
-        }
+        // if (random_int(0, 100) < (int)$percent) {
+        //     throw new InvalidRequestException('参与的用户过多，请稍后再试', 403);
+        // }
         
         return $next($request);
     }
